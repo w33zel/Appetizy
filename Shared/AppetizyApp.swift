498d33d48ed831c175aa26847fee3b8f9390e428
w33zel <fu.please@gmail.com>
2020-11-13 16:21:44 +0100

    Initial Commit

---
+++ Shared/AppetizyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizyApp.swift
+//  Shared
+//
+//  Created by Christian Arlt on 13.11.20.
+//
+
+import SwiftUI
+
+@main
+struct AppetizyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
